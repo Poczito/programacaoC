6496d36894b63729c6318e19faae3b8eab940104
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2025-02-04 00:48:56 +0000

    Criei arquivo que introduz o tópico dos modificadores de tipo de dados

---
+++ modificadores.c
@@ -0,0 +1,23 @@
+//Função: explicar, testar e exemplificar os modificadores de tipo de variável
+#include <stdio.h>
+
+int main(){
+
+    /*
+    //signed x unsigned
+
+    int numeroSinal = 3000000000; // Este valor excede o limite de um int signed (normal)
+    unsigned int numeroSemSinal = 3000000000; // Modificador exclui a possibilidade de valores negativos, assim o limite de números é maior
+
+    printf("Número com sinal: %d\n", numeroSinal);
+    printf("Número sem sinal: %u\n", numeroSemSinal);
+    */
+
+    //long int
+
+    
+
+
+
+    return 0;
+}
