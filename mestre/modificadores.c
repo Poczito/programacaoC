4d118256f4d214f36a9c49b57a33dabce383dfce
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2025-02-05 19:46:22 +0000

    Atualizações em modificadores.c

--- mestre/modificadores.c
+++ mestre/modificadores.c
@@ -24,6 +24,16 @@ int main(){
     printf("Número grande + 1: %ld\n", numeroGrande + 1);
     */
 
+    /*
+    //long double - números ainda mais precisos
+    double numeroPreciso = 3.141592653589793;
+    long double numeroMuitoPreciso = 3.141592653589793238463;
+
+    printf("Número preciso (double): %.15f\n", numeroPreciso);
+    printf("Número muito preciso (long double): %.21Lf\n", numeroMuitoPreciso);
+    */
+    
+    /*
     //Verificando o tamanho das variáveis
     printf("Tamanho de int: %u bytes\n", sizeof(int)); //sizeof retorna o tamanho em bytes da variável
     printf("Tamanho de unsigned int: %u bytes\n", sizeof(unsigned int));
@@ -32,6 +42,28 @@ int main(){
     printf("Tamanho de float: %u bytes\n", sizeof(float));
     printf("Tamanho de double: %u bytes\n", sizeof(double));
     printf("Tamanho de long double: %u bytes\n", sizeof(long double));
+    */
+
+    /*
+    //Testes juntando unsigned e long int
+    unsigned long int numeroGrandePositivo = 4000000000;
+    unsigned int numeroPositivo = 4000000000;
+    long int numeroGrande = 4000000000;
+    int numero = 4000000000;
+
+    printf("Número positivo grande: %lu\n", numeroGrandePositivo);
+    printf("Número positivo: %u\n", numeroPositivo);
+    printf("Número grande: %ld\n", numeroGrande);
+    printf("Número: %d\n", numero);
+    */
+
+    //Variáveis do tipo short
+    short int numeroPequeno = 32767; //Valor máximo de short int;
+
+    printf("Número pequeno (short int): %d\n", numeroPequeno);
+    numeroPequeno += 1;
+    printf("Número pequeno + 1: %d\n", numeroPequeno);
+    printf("Tamanho de short int: %u bytes\n", sizeof(short int));
 
     return 0;
 }
