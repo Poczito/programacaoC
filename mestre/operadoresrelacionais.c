ca0a988f7eca8fd27952e813a062761a725c5892
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2025-02-12 17:27:41 +0000

    Criação de operadoresrelacionais

---
+++ mestre/operadoresrelacionais.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(){
+
+    /*  Operadores relacionais
+    a > b (maior que)
+    a < b (menor que)
+    a >= b (maior ou igual a)
+    a <= b (menor ou igual a)
+    a == b (igual a)
+    a != b (diferente de)
+    */
+
+    /* Primeiros exemplos
+    int a = 20;
+    float b = 20.0;
+
+    printf("a = %d e b = %f\n", a, b);
+    printf("a >= b: %d\n", a >= b);
+    printf("a <= b: %d\n", a <= b);
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n\n", a != b);
+    */
+
+    /* Exemplos com char
+    int a = 20;
+    float b = 20.0;
+    char c = 'a';
+
+    printf("a = %d, b = %f e c = %c ou c = %d\n", a, b, c, c);
+    printf("a >= c: %d\n", a >= c);
+    printf("a <= c: %d\n", a <= c);
+    printf("a == c: %d\n", a == c);
+    printf("a != c: %d\n", a != c);
+    */
+
+    /* Conversão implícita e explícita
+
+    float a = 10.2;
+    int b = 10;
+
+    printf("a = %.1f e b = %d\n", a, b);
+    printf("a > b: %d\n", a > b);
+    printf("a == b: %d\n", a == b);
+    printf("int a > b: %d\n", (int) a > b);
+    printf("int a == b: %d\n", (int) a == b);
+
+    */
+   
+    return 0;
+}
