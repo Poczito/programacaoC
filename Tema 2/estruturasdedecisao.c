03afbeba35b4787ca3426ddb708d9a4d06ca6bf3
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2025-02-18 17:39:49 +0000

    Organização das pastas pra criação da pasta tema 2 Criação do documento sobre estruturas de decisão Inclusão das estruturas if e else-if

---
+++ Tema 2/estruturasdedecisao.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+
+    /*
+    //Estrutura If >> if(condição){comando a ser executado caso condição verdadeira}
+    char nome[50];
+    int idade;
+
+    printf("Digite o seu nome: ");
+    scanf("%s", &nome);
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    if (idade >= 18){
+        printf("Você é maior de idade!\n");
+    }
+
+    */
+   
+    /*
+
+    //Exemplo de estrutura if-else
+    char nome[50];
+    float nota;
+
+    printf("Insira o nome do aluno: ");
+    scanf("%s", &nome);
+    printf("Insira a nota do aluno: ");
+    scanf("%f", &nota);
+    if ((float) nota >= 6)
+    {
+        printf("O aluno %s passou com nota %.2f", nome, nota);
+    } else {
+        printf("O aluno %s não passou, pois obteve nota menor que 6.0", nome);
+    }
+    */
+
+    return 0;
+}
