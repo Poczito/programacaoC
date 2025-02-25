c01f725236c79b7c0739906faab83ea99e9dd8e5
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2025-02-25 04:40:26 +0000

    Criação do operadorternario.c

---
+++ Tema 2/operadorternario.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 18;
+
+    // idade >= 18 ? printf("É maior de idade!\n") : printf("Não é maior de idade!\n");
+
+    /* int resultado;
+
+    resultado = idade >= 18 ? 1 : 0;
+
+    if(resultado == 1){
+        printf("Você é maior de idade!");
+    } else {
+        printf("Você não é maior de idade!");
+    }
+    
+    */
+
+    int numero1 = 20, numero2 = 21;
+
+    (numero1 > numero2) ? printf("O número %d é maior", numero1) : printf("O número %d é maior", numero2);
+
+    return 0;
+}
