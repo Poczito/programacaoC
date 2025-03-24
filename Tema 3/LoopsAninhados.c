ff106dc4df58801a6a2e55dbefdee71f5732f25b
Celso de Oliveira Júnior <117027747+Poczito@users.noreply.github.com>
2025-03-24 11:44:42 +0000

    Criação do LoopsAninhados e pratica_aventureiro

--- Tema 3/LoopsAninhados.c
+++ Tema 3/LoopsAninhados.c
@@ -2,6 +2,10 @@
 
 int main(){
 
+    // Tabuada
+
+    /*
+
     int i = 1;
 
     while (i <= 10){
@@ -11,6 +15,15 @@ int main(){
             j++;
         } while (j <= 10);
         i++;
+        printf("\n");
+    }
+
+    */
+
+    for (int i = 1; i <= 10; i++){
+        for (int j = 1; j <= 10; j++){
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
         printf("\n");
     }
 
